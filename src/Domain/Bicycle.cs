1b096cb352bdce68d9e332d17b55ecfabb76f907
Benjamin Vertonghen <benjamin.vertonghen@hogent.be>
2021-09-05 11:09:44 +0200

    Finalize

---
+++ src/Domain/Bicycle.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Domain;
+public class Bicycle
+{
+    private int gear = 5;
+    public void Brake(){
+        System.Console.WriteLine("I'm braking in gear: " + gear);
+    }
+}
